18f7926b0da4a0671243edc33a1c8176a5d8e2f5
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 14:26:11 -0800

    add comment

--- python_ex103.py
+++ python_ex103.py
@@ -1,13 +1,18 @@
+# I shortened the message that gets printed in this example.
+# I also shortened the variable names.
+# I did this so the example will fit better on smaller devices.
+# If you have an older version of the book, you can email
+# me at cory@theselftaughtprogrammer.io 
+# to update to the newest version. Thank you so much for reading!
 
-# http://tinyurl.com/h5gw993
 
 def even_odd():
-    number = input("type a number")
-    number = int(number)
-    if number % 2 == 0:
-        print("The number is even.")
+    n = input("type a number")
+    n = int(n)
+    if n % 2 == 0:
+        print("n is even.")
     else:
-        print("The number is odd.")
+        print("n is odd.")
 
 
 even_odd()
