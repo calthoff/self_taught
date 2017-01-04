22e884bc1b48aa3b5049133b001b286c9f78b3c4
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-03 21:46:32 -0800

    fix hangman

--- python_ex239.py
+++ python_ex239.py
@@ -1,24 +1,16 @@
-
- # http://tinyurl.com/hshb567
-
-
 def hangman(word):
     wrong = 0
-   stages = ["",
-             "________
-             ",
-             "|      |      ",
-             "|      0
-          ", "|     /|\     ",
-             "|     / \     ",
-             "|
-            "]
-
-
-
-
+    stages = ["",
+             "________        ",
+             "|               ",
+             "|        |      ",
+             "|        0      ",
+             "|       /|\     ",
+             "|       / \     ",
+             "|               "
+              ]
     letters_left = list(word)
-    letter_board = ["__"] * len(word)
+    board = ["__"] * len(word)
     win = False
     print("Welcome to Hangman")
     while wrong < len(stages) - 1:
@@ -26,22 +18,21 @@ def hangman(word):
         guess = input("Guess a letter")
         if guess in letters_left:
             cindex = letters_left.index(guess)
-            letter_board[cindex] = guess
+            board[cindex] = guess
             letters_left[cindex] = '$'
         else:
             wrong += 1
-        print((" ".join(letter_board)))
+        print((" ".join(board)))
         print("\n".join(stages[0: wrong + 1]))
-        if "__" not in letter_board:
-            print("You win! The word was:")
-            print(" ".join(letter_board))
+        if "__" not in board:
+            print("""You lose! It was {}."""
+                  .format(word))
+            print(" ".join(board))
             win = True
             break
     if not win:
         print("\n".join(stages[0: wrong]))
-        print("""You lose!
-     The words
-     was {}""".format(word))
-
+        print("""You lose! It was {}."""
+              .format(word))
 
 hangman("cat")
