b2f106790d30ea73dd8de1f6f5c45b2a620749f3
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 14:34:19 -0800

    shortened message

--- python_ex112.py
+++ python_ex112.py
@@ -1,9 +1,13 @@
+# I shortened the message that gets printed in this example.
+# The example in older versions of the book prints "Type another number:"
+# I did this so the example will fit better on smaller devices.
+# If you have an older version of the book, you can email
+# me at cory@theselftaughtprogrammer.io 
+# to update to the newest version. Thank you so much for reading!
 
-# http://tinyurl.com/znev97y
 
-
-a = input("type a number")
-b = input("type another number")
+a = input("type a number:")
+b = input("type another:")
 a = int(a)
 b = int(b)
 print(a / b)
