62c0e8d6c78233c90b5bf8043ce41a9a01950a15
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 13:56:08 -0800

    home name change

--- python_ex76.py
+++ python_ex76.py
@@ -1,17 +1,17 @@
+# the variable in this example has been changed from
+# country in older versions of the book to home
+# so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io
+# to update to the newest version. Thank you so much for reading!
 
-
-
-# https://goo.gl/L0OorN
-
-
-country = "Thailand"
-if country == "Japan":
+home = "Thailand"
+if home == "Japan":
     print("Hello, Japan!")
-elif country == "Thailand":
+elif home == "Thailand":
     print("Hello, Thailand!")
-elif country == "India":
+elif home == "India":
     print("Hello, India!")
-elif country == "China":
+elif home == "China":
     print("Hello, China!")
 else:
     print("Hello, World!")
