cf3d8cd1874807a2eaca9e7f95672ea6ac933dd5
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 13:53:18 -0800

    change variable name to home comment

--- python_ex72.py
+++ python_ex72.py
@@ -1,9 +1,11 @@
+# the variable in this example has been changed from
+# country in older versions of the book to home
+# so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io
+# to update
 
-# https://goo.gl/bd4LVW
-
-
-country = "Canada"
-if country == "America":
+home = "Canada"
+if home == "America":
     print("Hello, America!")
 else:
     print("Hello, World!")
