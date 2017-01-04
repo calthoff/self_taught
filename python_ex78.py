292c75e1a8f3d542f8f56adca3cdf101f4559696
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 14:01:12 -0800

    shorter message printed comment

--- python_ex78.py
+++ python_ex78.py
@@ -1,6 +1,9 @@
-
-# https://goo.gl/tgcmXN
-
+# I shortened the message that gets printed in this example.
+# Older versions of the book print "I don't know what x is."
+# I did this so the example will fit better on smaller devices.
+# If you have an older version of the book, you can email
+# me at cory@theselftaughtprogrammer.io 
+# to update to the newest version. Thank you so much for reading!
 
 x = 100
 if x == 10:
@@ -8,7 +11,7 @@ if x == 10:
 elif x == 20:
     print("20!")
 else:
-    print("I don’t know what x is!")
+    print("I don’t know!")
 
 
 if x == 100:
