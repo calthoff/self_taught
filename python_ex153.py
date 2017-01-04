cebf3d68d393320aba76d6350476e50db583efc3
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 15:18:04 -0800

    error message

--- python_ex153.py
+++ python_ex153.py
@@ -1,18 +1,19 @@
-
-# http://tinyurl.com/h7e7oz4
-
+# IMPORTANT. In some previous versions of the book, this example contains an error.
+# I've fixed the error in the newest version of the book and it is fixed below.
+# If you have an older version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
 rhymes = {"1": "fun",
           "2": "blue",
           "3": "me",
-          "4": "floor"
+          "4": "floor",
           "5": "live"
           }
 
 
 n = input("Type a number:")
-if n in songs:
+if n in rhymes:
     rhyme = rhymes[n]
     print(rhyme)
 else:
-    print("Couldn’t find it.")
+    print("Not found.")
