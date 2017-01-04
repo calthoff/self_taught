7318d0e496040f7403560c9808e9b434871a0b3d
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 15:07:30 -0800

    shortened

--- python_ex152.py
+++ python_ex152.py
@@ -1,9 +1,14 @@
+# I changed one of the keys in this example from
+#  "Animal Farm" in older versions of the book to "The Trial" 
+# so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
 books = {"Dracula": "Stoker",
          "1984": "Orwell",
-         "Animal Farm": "Orwell"}
+         "The Trial": "Kafka"}
 
 
-del books["Animal Farm"]
+del books["The Trial"]
 
 books
