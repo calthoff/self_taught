20d159971dd32ff4f6999b4dba1640b916b2c8d8
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 14:59:09 -0800

    fix spacing

--- python_ex142.py
+++ python_ex142.py
@@ -7,4 +7,5 @@
 dys = ("1984",
             "Brave New World",
             "Fahrenheit 451")
+
 dys[1] = "Handmaid's Tale"
--- python_ex144.py
+++ python_ex144.py
@@ -8,4 +8,5 @@
 dys = ("1984",
        "Brave New World",
        "Fahrenheit 451")
+
 "1984" in dys
