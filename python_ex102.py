220d55cc6604ec907b02301974737a4a23d916a0
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 14:22:56 -0800

    shortened comment

--- python_ex102.py
+++ python_ex102.py
@@ -1,28 +1,31 @@
+# I shortened the message that gets printed in this example.
+# I also shortened the variable names.
+# I did this so the example will fit better on smaller devices.
+# If you have an older version of the book, you can email
+# me at cory@theselftaughtprogrammer.io 
+# to update to the newest version. Thank you so much for reading!
 
-# http://tinyurl.com/zm68k89
+n = input("type a number")
+n = int(n)
 
 
-number = input("type a number")
-number = int(number)
-
-
-if number % 2 == 0:
-    print("The number is even.")
+if n % 2 == 0:
+    print("n is even.")
 else:
-    print("The number is odd.")
+    print("n is odd.")
 
 
-number = input("type a number")
-number = int(number)
-if number % 2 == 0:
-    print("The number is even.")
+n = input("type a number")
+n = int(n)
+if n % 2 == 0:
+    print("n is even.")
 else:
-    print("The number is odd.")
+    print("n is odd.")
 
 
-number = input("type a number")
-number = int(number)
-if number % 2 == 0:
-    print("The number is even.")
+n = input("type a number")
+n = int(n)
+if n % 2 == 0:
+    print("n is even.")
 else:
-    print("The number is odd.")
+    print("n is odd.")
