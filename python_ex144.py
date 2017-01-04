16e8306e2add270be4fe73ec7526e56bc7ba9e89
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 14:56:37 -0800

    shortened variable name

--- python_ex144.py
+++ python_ex144.py
@@ -1,5 +1,11 @@
+# I changed the variable in this example from
+#  dystopia in older versions of the book to dys 
+# so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
-dystopia = ("1984",
-            "Brave New World",
-            "Fahrenheit 451")
-"1984" in dystopia
+
+dys = ("1984",
+       "Brave New World",
+       "Fahrenheit 451")
+"1984" in dys
