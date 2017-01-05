aaaa73361ee09db442cf4137e91cebad5d643df3
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 17:06:58 -0800

    shorten variable

--- python_ex166.py
+++ python_ex166.py
@@ -1,4 +1,9 @@
+# I changed the variable in this example from
+#  author in older versions of the book to ff 
+# so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
-author = "F. Fitzgerald"
-author = "F. Scott Fitzgerald"
-author
+ff = "F. Fitzgerald"
+ff = "F. Scott Fitzgerald"
+ff
