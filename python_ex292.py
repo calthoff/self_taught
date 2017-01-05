1e36696aadfb0e760a11de0631b19b8d41900bf1
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-05 13:28:11 -0800

    change example

--- python_ex292.py
+++ python_ex292.py
@@ -1,9 +1,19 @@
+# IMPORTANT. I recently changed this example in the book.
+# I made changes so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
+
 
 def scrape(self):
-    r = urllib.request.urlopen(self.site)
+    r = urllib.request\
+        .urlopen(self.site)
     html = r.read()
-    soup = BeautifulSoup(html, "html.parser")
-    for tag in soup.find_all("a"):
+    parser = "html.parser"
+    sp = BeautifulSoup(html,
+                       parser)
+    for tag in sp.find_all("a"):
         url = tag.get("href")
-        if url and "html" in url:
+        if url is None:
+            continue
+        if "html" in url:
             print("\n" + url)
