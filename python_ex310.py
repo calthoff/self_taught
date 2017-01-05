4cbc1fc4b2719f38570444e9fdb95bef8d6b8156
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-05 14:33:11 -0800

    change spacing

--- python_ex310.py
+++ python_ex310.py
@@ -1,26 +1,34 @@
-
 def bottles_of_beer(bob):
-    """ Prints 99 Bottle of Beer on
-    the Wall lyrics.
-    :param bob: Must be a
-    positive integer.
+    """ Prints 99 Bottle 
+        of Beer on the
+        Wall lyrics.
+        :param bob: Must
+        be a positive 
+        integer.
     """
     if bob < 1:
-        print("""No more bottles
-              of beer on the wall.
-              No more bottles of
-              beer.""")
+        print("""No more 
+                 bottles 
+                 of beer 
+                 on the wall. 
+                 No more 
+                 bottles of 
+                 beer.""")
         return
     tmp = bob
     bob -= 1
-    print("""{} bottles of beer on the wall.
-             {} bottles of beer. Take one down,
-             pass it around, {} bottles of beer
-             on the wall.
-            """.format(tmp, tmp, bob))
+    print("""{} bottles of 
+             beer on the 
+             wall. {} bottles
+             of beer. Take one 
+             down, pass it 
+             around, {} bottles 
+             of beer on the 
+             wall.
+          """.format(tmp, 
+                     tmp, 
+                     bob))
     bottles_of_beer(bob)
-
-
 
 
 bottles_of_beer(99)
