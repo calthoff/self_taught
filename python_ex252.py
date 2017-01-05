36d8bed2f312ebe072337dd18e1fb48a11edc394
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 20:24:23 -0800

    change  variable

--- python_ex252.py
+++ python_ex252.py
@@ -1,13 +1,14 @@
-
-# http://tinyurl.com/hv74cs4
+# I changed the variable in this example from
+#  length in older versions of the book to len 
+# so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
 
 class Rectangle():
     def __init__(self, w, l):
         self.width = w
         self.length = l
-
-
 
     def area(self):
         return self.width * self.length
