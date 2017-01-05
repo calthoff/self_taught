affa5ca77c379d4172026ffa2623e16466cb85f6
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 18:46:06 -0800

    change example

--- python_ex198.py
+++ python_ex198.py
@@ -1,5 +1,17 @@
+# IMPORTANT. I recently changed this example in the book.
+# In older versions of the book the example was
 
-ivan = """In place of death
-          there was light.
-       """
+#ivan = """In place of death
+#          there was light.
+#       """
+#ivan[17:]
+
+
+# I made this change so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
+
+ivan = """In place of death there \
+was light."""
+
 ivan[17:]
