b6d1bfe2f2c6e58d5b0db324e5cd2fa66208a463
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 20:19:33 -0800

    change  example

--- python_ex251.py
+++ python_ex251.py
@@ -1,19 +1,21 @@
-
-# http://tinyurl.com/jn9cptz
+# IMPORTANT. I changed many of the variable names in this example
+# so the example fits on smaller devices. Old versions of the book have longer
+# variable names. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
 
 class Rectangle():
     def __init__(self, w, l):
         self.width = w
-        self.length = l
+        self.len = l
 
     def area(self):
-        return self.width * self.length
+        return self.width * self.len
 
     def change_size(self, w, l):
         self.width = w
-        self.length = l
-
+        self.len = l
 
 rectangle = Rectangle(10, 20)
 print(rectangle.area())
