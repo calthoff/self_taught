13c19299facf329bfae30da2e866b1a534a71804
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 19:14:17 -0800

    change variable name

--- python_ex236.py
+++ python_ex236.py
@@ -1,3 +1,8 @@
+# IMPORTANT. I changed many of the variable names in this example
+# so the example fits on smaller devices. Old versions of the book have longer
+# variable names. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
 # http://tinyurl.com/hshb567
 
@@ -5,17 +10,15 @@
 def hangman(word):
     wrong = 0
     stages = ["",
-             "________",
-             "",
-             "|      |      ",
-             "|      0      ",
-             "|     /|\     ",
-             "|     / \     ",
-             "|"             ,
-             ]
-    letters_left = list(word)
-    letter_board = ["__"] * len(word)
+             "________        ",
+             "|               ",
+             "|        |      ",
+             "|        0      ",
+             "|       /|\     ",
+             "|       / \     ",
+             "|               "
+              ]
+    rletters = list(word)
+    board = ["__"] * len(word)
     win = False
-    print('Welcome to Hangman')
-
-hangman("hello")
+    print("Welcome to Hangman")
