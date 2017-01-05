4288caa0e364a5c605216b31f36e2cca347b11e0
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-05 12:59:43 -0800

    changes

--- python_ex276.py
+++ python_ex276.py
@@ -1,17 +1,23 @@
+# I split the line of code self.cards.append(Card(i, j)) in early versions of the book into two lines:
+# self.cards\
+# .append(Card(i,
+#              j))
+# Email cory@theselftaughtprogrammer.io for latest version
+
 
 from random import shuffle
 
-
 class Deck:
-   def __init__(self):
-       self.cards = []
-       for i in range(2, 15):
-           for j in range(4):
-               self.cards.append(Card(i, j))
-       shuffle(self.cards)
+    def __init__(self):
+        self.cards = []
+        for i in range(2, 15):
+            for j in range(4):
+                self.cards\
+                    .append(Card(i,
+                                 j))
+        shuffle(self.cards)
 
-
-   def rm_card(self):
-       if len(self.cards) == 0:
-           return
-       return self.cards.pop()
+    def rm_card(self):
+        if len(self.cards) == 0:
+            return
+        return self.cards.pop()
