2ebdffb8ca2ad81dedb07df5bcde341bbe617ab1
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 19:22:35 -0800

    changed example

--- python_ex239.py
+++ python_ex239.py
@@ -1,3 +1,10 @@
+# IMPORTANT. I changed the way this example works
+# so it will fit on smaller devices. Old versions of the book have
+# a different example. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
+
+
 def hangman(word):
     wrong = 0
     stages = ["",
@@ -9,29 +16,34 @@ def hangman(word):
              "|       / \     ",
              "|               "
               ]
-    letters_left = list(word)
+    rletters = list(word)
     board = ["__"] * len(word)
     win = False
     print("Welcome to Hangman")
     while wrong < len(stages) - 1:
         print("\n")
-        guess = input("Guess a letter")
-        if guess in letters_left:
-            cindex = letters_left.index(guess)
-            board[cindex] = guess
-            letters_left[cindex] = '$'
+        msg = "Guess a letter"
+        char = input(msg)
+        if char in rletters:
+            cind = rletters \
+                   .index(char)
+            board[cind] = char
+            rletters[cind] = '$'
         else:
             wrong += 1
         print((" ".join(board)))
-        print("\n".join(stages[0: wrong + 1]))
+        e = wrong + 1
+        print("\n"
+              .join(stages[0: e]))
         if "__" not in board:
-            print("You win!"
-                  .format(word))
+            print("You win!")
             print(" ".join(board))
             win = True
             break
     if not win:
-        print("\n".join(stages[0: wrong]))
+        print("\n"
+              .join(stages[0: \
+              wrong]))
         print("You lose! It was {}."
               .format(word))
 
