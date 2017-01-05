bb70399a50b91829223278a98f25ed0af9812673
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-05 12:21:33 -0800

    change spacing and variable

--- python_ex261.py
+++ python_ex261.py
@@ -5,23 +5,23 @@
 class Shape():
     def __init__(self, w, l):
         self.width = w
-        self.length = l
+        self.len = l
 
     def print_size(self):
         print("""{} by {}
-         """.format(self.width,
-                    self.length))
+              """.format(self.width,
+                         self.len))
 
 
 class Square(Shape):
     def area(self):
-        return self.width * self.length
+        return self.width * self.len
 
     def print_size(self):
-        print("""I am {} by {}
-              """.format(self.width,
-                         self.length))
-
+      print("""I am {} by {}
+            """.format(self.width,
+                       self.len))
 
 a_square = Square(20, 20)
 a_square.print_size()
+
