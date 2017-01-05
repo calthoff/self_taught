2b27ac0d37447ceeb4c3326de713e58fc8845042
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 19:31:46 -0800

    change example

--- python_ex244.py
+++ python_ex244.py
@@ -1,6 +1,10 @@
+# I changed this example to print Created. In previous examples it printed
+# Orange object created! I did this so the example fits on smaller devices.
+# If you have an older version of the book, you can email me at
+# cory@theselftaughtprogrammer.io  and I will send you the newest version.
+# Thank you so much for purchasing my book!
 
-# http://tinyurl.com/z2x4ewy
+
 class Orange:
     def __init__(self):
-        print("""Orange object
-            created!""")
+        print("Created!")
