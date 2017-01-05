5e7f87f193af1382c1d8f87d37cb818539ef54bb
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 19:35:12 -0800

    change example

--- python_ex247.py
+++ python_ex247.py
@@ -1,21 +1,17 @@
-
-# http://tinyurl.com/gmhm33k
+# IMPORTANT. I changed many of the variable names in this example
+# so the example fits on smaller devices. Old versions of the book have longer
+# variable names. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
 
 class Orange:
-    def __init__(self, weight, color):
-        self.weight = weight
-        self.color = color
-        print("""Orange object
-                 created!""")
-
-
-
+    def __init__(self, w, c):
+        self.weight = w
+        self.color = c
+        print("Created!")
 
 or1 = Orange(10, "dark orange")
-
-
-
 
 print(or1.weight)
 print(or1.color)
