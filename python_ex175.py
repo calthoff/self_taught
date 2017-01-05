a53754bb8c88e77d350349f22adb8fdd5538586a
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 17:16:02 -0800

    new example

--- python_ex175.py
+++ python_ex175.py
@@ -1,12 +1,25 @@
+# IMPORTANT. I recently changed this example in the book.
+# In older versions of the book the example was:
 
-name = "Walter Isaacson"
-year_born = "1952"
-book1 = "Steve Jobs"
-book2 = "Benjamin Franklin"
+#name = "Walter Isaacson"
+#year_born = "1952"
+#book1 = "Steve Jobs"
+#book2 = "Benjamin Franklin"
+
+#"""{} was born in {}.
+#   He's written books about
+#   {} and {}.
+#""".format(name, year_born,
+#            book1, book2)
+
+# I made this change so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
 
-"""{} was born in {}.
-   He's written books about
-   {} and {}.
-""".format(name, year_born,
-            book1, book2)
+author = "William Faulkner"
+year_born = "1897"
+
+"""{} was born in {}."""\
+   .format(author,
+           year_born)
