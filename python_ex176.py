bfd72a5999d5d547ee17db27a982819c04044c59
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 17:18:48 -0800

    shorten

--- python_ex176.py
+++ python_ex176.py
@@ -1,14 +1,19 @@
+# I shortened this example.
+# The example in older versions of the book prints "Type another number:"
+# I did this so the example will fit better on smaller devices.
+# If you have an older version of the book, you can email
+# me at cory@theselftaughtprogrammer.io 
+# to update to the newest version. Thank you so much for purchasing my book!
 
-noun1 = input("Enter a noun:")
-verb = input("Enter a verb:")
-adj = input("Enter an adjective:")
-noun2 = input("Enter a noun:")
+n1 = input("Enter a noun:")
+v = input("Enter a verb:")
+adj = input("Enter an adj:")
+n2 = input("Enter a noun:")
 
+r = """The {} {} the {} {}
+    """.format(n1,
+               v,
+               adj,
+               n2)
 
-answer = """The {} {} the
-         {} {}
-         """.format(noun1,
-              verb,
-   adj,
-   noun2)
-print(answer)
+print(r)
