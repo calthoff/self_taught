8b081df4705e4d5008b1b9c46d980fc1b42e746b
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-05 13:45:55 -0800

    change example

--- python_ex294.py
+++ python_ex294.py
@@ -1,24 +1,28 @@
+# IMPORTANT. I recently changed this example in the book.
+# I changed return self.items[len(self.items)-1] (in older versions) to:
+# last = len(self.items)-1
+# return self.items[last]
+# I made this change so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
+
 
 class Stack:
     def __init__(self):
         self.items = []
 
-
     def is_empty(self):
         return self.items == []
-
 
     def push(self, item):
         self.items.append(item)
 
-
     def pop(self):
-        return self.items.pop()[i]
-
+        return self.items.pop()
 
     def peek(self):
-        return self.items[len(self.items)-1]
-
+        last = len(self.items)-1
+        return self.items[last]
 
     def size(self):
         return len(self.items)
