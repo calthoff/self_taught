06b7c5adc731e7dbaab91a54bae4af20064bc1a2
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 17:04:47 -0800

    shorten variable

--- python_ex160.py
+++ python_ex160.py
@@ -1,20 +1,22 @@
+# I changed the variable in this example from
+#  new_yrok in older versions of the book to ny 
+# so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
-# http://tinyurl.com/zpqme8a
+ny = {"location":
+      (40.7128,
+       74.0059),
 
 
-new__york = {"location":
-            (40.7128,
-            74.0059),
+      "celebs":
+      ["W. Allen",
+       "Jay Z",
+       "K. Bacon"],
 
-
-             "celebs":
-             ["W. Allen",
-              "Jay Z",
-              "K. Bacon"],
-
-             "facts":
-             {"state":
-              "NY",
-              "country":
-              "America"}
+      "facts":
+      {"state":
+       "NY",
+       "country":
+       "America"}
 }
