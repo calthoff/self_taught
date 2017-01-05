a52e4fac92e8f77baa4243f45f66c27961ac76d0
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-05 12:27:17 -0800

    variable message

--- python_ex263.py
+++ python_ex263.py
@@ -1,6 +1,12 @@
+# variable name French Bulldog in older editions of the book changed to Bulldog so th example fits on small screens.
+# Email cory@theselftaughtprogrammer.io for latest version
+
+
+# Continue from
+# last example
 
 mick = Person("Mick Jagger")
 stan = Dog("Stanley",
-           "French Bulldog",
+           "Bulldog",
            mick)
 print(stan.owner.name)
