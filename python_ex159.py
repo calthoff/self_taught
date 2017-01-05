c2f95848e8d5f1aef4186db0decbccc4abf02eeb
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 17:03:32 -0800

    shorten variable

--- python_ex159.py
+++ python_ex159.py
@@ -1,14 +1,17 @@
-
-# http://tinyurl.com/zw3xnhu
-
-
-author_bday = {"Hemingway":
-               "7.21.1899",
-               "Fitzgerald":
-               "9.24.1896"}
+# I changed the variable in this example from
+#  author_bday in older versions of the book to bday 
+# so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
 
-my_list = [author_bday]
+bday = {"Hemingway":
+        "7.21.1899",
+        "Fitzgerald":
+        "9.24.1896"}
+
+
+my_list = [bday]
 print(my_list)
-my_tuple = (author_bday,)
+my_tuple = (bday,)
 print(my_tuple)
