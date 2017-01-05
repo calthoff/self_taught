2c67d65a04dce6ff7744055dc38ed98aadab9e1c
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 20:40:33 -0800

    change example

--- python_ex258.py
+++ python_ex258.py
@@ -1,3 +1,10 @@
+# I changed the variable in this example from
+#  length in older versions of the book to len 
+# so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
+
+
 class Shape():
     def __init__(self, w, l):
         self.width = w
@@ -5,8 +12,11 @@ class Shape():
 
     def print_size(self):
         print("""{} by {}
-              """.format(self.width,
-                         self.len))
+             """.format(self.width,
+                        self.len))
 
-my_shape = Shape(20, 25)
-my_shape.print_size()
+class Square(Shape):
+    pass
+
+a_square = Square(20,20)
+a_square.print_size()
