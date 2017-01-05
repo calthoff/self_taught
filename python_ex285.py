0fb0f8a8322fabeb1bc843dcccac9543189a0441
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-05 13:18:14 -0800

    change spacing

--- python_ex285.py
+++ python_ex285.py
@@ -1,29 +1,19 @@
-
-# http://tinyurl.com/hpx8696
-
 import re
 
 
-
-
-text = """Giraffes have aroused the
-          curiosity of
-          __PLURAL_NOUN__ since
-          earliest times. The
-          giraffe is the tallest
-          of all living
-          __PLURAL_NOUN__,
-          but scientists are
-          unable to explain how it
-          got its long
-          __PART_OF_THE_BODY__.
-          The giraffe's tremendous height,
-          which might reach __NUMBER__
-          __PLURAL_NOUN__,comes
-          from it legs and __BODYPART__.
-      """
-
-
+text = """Giraffes have aroused
+ the curiosity of __PLURAL_NOUN__
+ since earliest times. The
+ giraffe is the tallest of all
+ living __PLURAL_NOUN__, but
+ scientists are unable to
+ explain how it got its long
+ __PART_OF_THE_BODY__. The
+ giraffe's tremendous height,
+ which might reach __NUMBER__
+ __PLURAL_NOUN__, comes from
+ it legs and __BODYPART__.
+"""
 
 
 def mad_libs(mls):
@@ -37,19 +27,20 @@ def mad_libs(mls):
     __hint_hint__ only
     __hint__.
     """
-    hints = re.findall("__.*?__", mls)
+    hints = re.findall("__.*?__",
+                      mls)
     if hints is not None:
         for word in hints:
-            new = input("""
-                        enter a {}
-                        """.format(word))
+            q = "Enter a {}"\
+                   .format(word)
+            new = input(q)
             mls = mls.replace(word,
-                                            new,
-   1)
+                              new,
+                              1)
         print('\n')
+        mls = mls.replace("\n", "")
         print(mls)
     else:
         print("invalid mls")
-
 
 mad_libs(text)
