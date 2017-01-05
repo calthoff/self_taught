b548d84f0685dc2d2d5bd9e5f90fb0618bb1e816
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 19:08:48 -0800

    fix error

--- python_ex234.py
+++ python_ex234.py
@@ -8,8 +8,7 @@
 import csv
 
 
-with open("st.csv",
-     "w") as f:
+with open("st.csv", "w") as f:
     write = csv.writer(f, delimiter=",")
     write.writerow(["one",
                     "two",
