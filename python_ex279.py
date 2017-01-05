039c5be267ae8d44a4b75fd19f8ef0f7eaf87653
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-05 13:02:56 -0800

    changes

--- python_ex279.py
+++ python_ex279.py
@@ -1,47 +1,64 @@
 
+# IMPORTANT. I recently made changes to this example to make it fit on smaller screens. The example in older versions
+# of the book will be different than what is displayed below.
+# If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
+
+
 class Game:
-   def __init__(self):
-       name1 = input("p1 name ")
-       name2 = input("p2 name ")
-       self.deck = Deck()
-       self.p1 = Player(name1)
-       self.p2 = Player(name2)
+    def __init__(self):
+        name1 = input("p1 name ")
+        name2 = input("p2 name ")
+        self.deck = Deck()
+        self.p1 = Player(name1)
+        self.p2 = Player(name2)
 
+    def wins(self, winner):
+        w = "{} wins this round"
+        w = w.format(winner)
+        print(w)
 
-   def play_game(self):
-       cards = self.deck.cards
-       print("beginning War!")
-       response = None
-       while len(cards) >= 2:
-           response = input("""q to
-                            quit. Other
-                            key to
-                            play.""")
-           if response == 'q':
-               break
-           p1_card = self.deck.rm_card()
-           p2_card = self.deck.rm_card()
-           print("""{} drew {} {} drew {}
-           """.format(self.p1.name,
-                           p1_card,
-                           self.p2.name,
-                           p2_card))
-           if p1_card > p2_card:
-               self.p1.wins += 1
-               print("""{} wins this round
-                     """.format(self.p1.name))
-           else:
-               self.p2.wins += 1
-               print("""{} wins this round
-                     """.format(self.p2.name))
-       print("""The War is over.{} wins
-             """.format(self.winner(self.p1,
-                                    self.p2)))
+    def draw(self, p1n, p1c, p2n, p2c):
+        d = "{} drew {} {} drew {}"
+        d = d.format(p1n,
+                     p1c,
+                     p2n,
+                     p2c)
+        print(d)
 
+    def play_game(self):
+        cards = self.deck.cards
+        print("beginning War!")
+        while len(cards) >= 2:
+            m = "q to quit. Any " + \
+                "key to play:"
+            response = input(m)
+            if response == 'q':
+                break
+            p1c = self.deck.rm_card()
+            p2c = self.deck.rm_card()
+            p1n = self.p1.name
+            p2n = self.p2.name
+            self.draw(p1n,
+                      p1c,
+                      p2n,
+                      p2c)
+            if p1c > p2c:
+                self.p1.wins += 1
+                self.wins(self.p1.name)
+            else:
+                self.p2.wins += 1
+                self.wins(self.p2.name)
 
-   def winner(self, p1, p2):
-       if p1.wins > p2.wins:
-           return p1.name
-       if p1.wins < p2.wins:
-           return p2.name
-       return "It was a tie!"
+        win = self.winner(self.p1,
+                         self.p2)
+        print("War is over.{} wins"
+              .format(win))
+
+    def winner(self, p1, p2):
+        if p1.wins > p2.wins:
+            return p1.name
+        if p1.wins < p2.wins:
+            return p2.name
+        return "It was a tie!"
