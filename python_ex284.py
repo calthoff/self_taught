5a185f38110250b1447ab1c280b6b254bebdae8b
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-05 13:15:49 -0800

    change variable name

--- python_ex284.py
+++ python_ex284.py
@@ -1,16 +1,17 @@
-
-# http://tinyurl.com/gvajo3x
+# I changed the variable in this example from
+#  text in older versions of the book to t 
+# so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
 
 import re
 
 
+t = "__one__ __two__ __three__"
 
 
-text = "__one__ __two__ __three__"
-
-
-results = re.findall("__.*?__", text)
+results = re.findall("__.*?__", t)
 
 
 for match in results:
