d9e878c35f65c8d520a71d9cb26c4513538dd74e
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 17:02:03 -0800

    shorten variables

--- python_ex158.py
+++ python_ex158.py
@@ -1,15 +1,17 @@
+# I changed the variables in this example from
+#  eighteens and nineteens in older versions of the book to eights and nines 
+# so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
-# http://tinyurl.com/z2fwrfe
-
-
-eighteens = ["Edgar Allan Poe",
-             "Charles Dickens"]
-
-
-nineteens = ["Hemingway",
-             "Fitzgerald",
-             "Orwell"]
+eights = ["Edgar Allan Poe",
+          "Charles Dickens"]
 
 
-authors = (eighteens, nineteens)
+nines = ["Hemingway",
+         "Fitzgerald",
+         "Orwell"]
+
+
+authors = (eights, nines)
 print(authors)
