5161d158ac243220759f9a4a38a8d808eee2fe44
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 18:56:45 -0800

    change variable

--- python_ex216.py
+++ python_ex216.py
@@ -1,13 +1,15 @@
-
-# http://tinyurl.com/jxwl9oz
-
+# I changed the variable in this example from
+#  added_up in older versions of the book to added 
+# so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
 list1 = [1, 2, 3, 4]
 list2 = [5, 6, 7, 8]
-added_up = []
+added = []
 for i in list1:
     for j in list2:
-        added_up.append(i + j)
+        added.append(i + j)
 
 
-print(added_up)
+print(added)
