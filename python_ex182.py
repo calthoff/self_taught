2bfa2eb38ec7ccc0dfdfd1c1373b2bd725d5e9de
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 17:26:04 -0800

    change example

--- python_ex182.py
+++ python_ex182.py
@@ -1,8 +1,19 @@
+# IMPORTANT. I recently changed this example in the book.
+# In older versions of the book the example was
 
-equ = """All animals are
-         equal, but some
-         animals are more
-         equal than others.
-      """
+#equ = """All animals are
+#         equal, but some
+#         animals are more
+#         equal than others.
+#      """
+#equ = equ.replace("a", "@")
+#equ
+
+# I made this change so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
+
+
+equ = """All animals are equal."""
 equ = equ.replace("a", "@")
-equ
+print(equ)
