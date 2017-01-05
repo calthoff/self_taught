9f041cacceeae9e0490ae0c3bfd6f823dc310656
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 18:41:25 -0800

    line reak

--- python_ex196.py
+++ python_ex196.py
@@ -1,6 +1,18 @@
+# IMPORTANT. I recently changed this example in the book.
+# In older versions of the book the example was
 
-ivan = """In place of death
-          there was light.
-       """
+#ivan = """In place of death
+#          there was light.
+#       """
+#ivan[0:17]
+#ivan[17:33]
+
+# I made this change so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
+
+ivan = """In place of death there \
+was light."""
+
 ivan[0:17]
 ivan[17:33]
