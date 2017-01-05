d647c58569de6ac2d6296006d17ca40b0bd16589
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-05 12:51:33 -0800

    new variable

--- python_ex270.py
+++ python_ex270.py
@@ -1,16 +1,14 @@
-
-# http://tinyurl.com/gv7zwjx
+# variable name the_same_bob in older editions of the book changed to same_bob.
+# Email cory@theselftaughtprogrammer.io for latest version
 
 
 class Person:
     def __init__(self):
         self.name = 'Bob'
 
-
 bob = Person()
-the_same_bob = bob
-print(bob is the_same_bob)
-
+same_bob = bob
+print(bob is same_bob)
 
 another_bob = Person()
 print(bob is another_bob)
