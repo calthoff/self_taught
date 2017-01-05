20579e7c616c9ab2fce674aeb8049f859a18572a
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 20:38:30 -0800

    fix formatting

--- python_ex258.py
+++ python_ex258.py
@@ -1,17 +1,12 @@
-
-# http://tinyurl.com/h7zmz6f
-
-
 class Shape():
     def __init__(self, w, l):
         self.width = w
-        self.length = l
+        self.len = l
 
     def print_size(self):
         print("""{} by {}
-        """.format(self.width,
-                  self.length))
-
+              """.format(self.width,
+                         self.len))
 
 my_shape = Shape(20, 25)
 my_shape.print_size()
