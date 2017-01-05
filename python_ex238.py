3597ac5c3d5d491f4283e11758247e53e82dcc9c
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 19:19:30 -0800

    changed example

--- python_ex238.py
+++ python_ex238.py
@@ -1,9 +1,13 @@
-
+# IMPORTANT. I changed the way this example works
+# so it will fit on smaller devices. Old versions of the book have
+# a different example. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
 
 if not win:
-    print('\n'.join(wrong[0: stage]))
-    print('You lose! The words was {}'.format(word))
-
-
-hangman()
+    print("\n"
+          .join(stages[0: \
+          wrong]))
+    print("You lose! It was {}."
+          .format(word))
