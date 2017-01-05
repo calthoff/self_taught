256ad3cf1fe5db03b3243a65a787f3726886139f
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 17:24:29 -0800

    change example

--- python_ex181.py
+++ python_ex181.py
@@ -1,4 +1,14 @@
+# IMPORTANT. I recently changed this example in the book.
+# In older versions of the book the example was
 
-sentence = "    The cat’s craddle.        "
-sentence = sentence.strip()
-sentence
+# sentence = "    The cat’s craddle.        "
+# sentence = sentence.strip()
+# sentence
+
+# I made this change so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
+
+s = "    The        "
+s = s.strip()
+s
