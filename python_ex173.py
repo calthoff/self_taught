9bd34f795962f31d2e59a0b3043754d7b0b61a55
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 17:10:45 -0800

    change example

--- python_ex173.py
+++ python_ex173.py
@@ -1,4 +1,10 @@
+# IMPORTANT. I recently changed this example in the book.
+# In older versions of the book the example was:
+#  """Walter Isaacson
+#   was born
+#   in {}.""".format("1952")
+# I made this change so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
-"""Walter Isaacson
-   was born
-   in {}.""".format("1952")
+"""William {}""".format("Faulkner")
