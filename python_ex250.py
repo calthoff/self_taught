064bb02307e764e25fd9f9d51e73acff93cf20cb
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 20:18:58 -0800

    change  example

--- python_ex250.py
+++ python_ex250.py
@@ -1,20 +1,22 @@
-
-# http://tinyurl.com/j6rgcov
+# IMPORTANT. I changed many of the variable names in this example
+# so the example fits on smaller devices. Old versions of the book have longer
+# variable names. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
 
 class Orange():
-    def __init__(self):
+    def __init__(self, w, c):
         """weights are in oz"""
-        self.weight = 6
-        self.color = 'orange'
+        self.weight = w
+        self.color = c
         self.mold = 0
-
+        print("Created!")
 
     def rot(self, days, temp):
-        self.mold = days * (temp * .1)
+        self.mold = days * temp
 
-
-orange = Orange()
+orange = Orange(6, "orange")
 print(orange.mold)
 orange.rot(10, 98)
 print(orange.mold)
