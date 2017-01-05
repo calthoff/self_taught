d0f307d1f4fbedad1f52212fb93bebe330e673c4
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 20:28:32 -0800

    change  variable

--- python_ex253.py
+++ python_ex253.py
@@ -1,9 +1,13 @@
+# I changed the variable in this example from
+#  numbers in older versions of the book to nums 
+# so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
-# http://tinyurl.com/j46kkr7
+
 class Data:
     def __init__(self):
-        self.numbers = [1, 2, 3, 4, 5]
-
+        self.nums = [1, 2, 3, 4, 5]
 
     def change_data(self, index, n):
-        self.numbers[index] = n
+        self.nums[index] = n
--- python_ex254.py
+++ python_ex254.py
@@ -1,21 +1,20 @@
-
-# http://tinyurl.com/hhdpkqh
-
+# I changed the variable in this example from
+#  numbers in older versions of the book to nums 
+# so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
 class Data:
     def __init__(self):
-        self.numbers = [1, 2, 3, 4, 5]
-
+        self.nums = [1, 2, 3, 4, 5]
 
     def change_data(self, index, n):
-        self.numbers[index] = n
-
+        self.nums[index] = n
 
 data_one = Data()
-data_one.numbers[0] = 100
-print(data_one.numbers)
-
+data_one.nums[0] = 100
+print(data_one.nums)
 
 data_two = Data()
 data_two.change_data(0, 100)
-print(data_two.numbers)
+print(data_two.nums)
