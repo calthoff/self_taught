b77a95b1111468d3392e3e3eca6c2b0439f36e34
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 18:54:03 -0800

    change variable

--- python_ex212.py
+++ python_ex212.py
@@ -1,5 +1,8 @@
-
-# http://tinyurl.com/zczxgcf
+# I changed the variable in this example from
+#  questions in older versions of the book to qs 
+# so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
 
 qs = ["What is your name?",
@@ -9,7 +12,7 @@ qs = ["What is your name?",
 n = 0
 while True:
     print("Type q to quit")
-    answer = input(questions[n])
-    if answer == "q":
+    a = input(qs[n])
+    if a == "q":
         break
     n = (n + 1) % 3
