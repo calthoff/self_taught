8ad089253d32ffa192c0ffe744a8a11baf795848
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-05 14:34:15 -0800

    change spacing

--- python_ex311.py
+++ python_ex311.py
@@ -1,6 +1,9 @@
-
 if bob < 1:
-    print("""No more bottles of beer
-                   on the wall. No more
-                   bottles of beer.""")
+    print("""No more
+           bottles
+           of beer
+           on the wall.
+           No more
+           bottles of
+           beer.""")
     return
