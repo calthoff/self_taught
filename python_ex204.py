56acdd7afb4219ab401f3e5f5e5dd9df91b9ed22
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 18:50:16 -0800

    variable change

--- python_ex204.py
+++ python_ex204.py
@@ -1,15 +1,17 @@
-
-# http://tinyurl.com/jdk7w3t
-
+# I changed the variable in this example from
+#  new_shows in older versions of the book to new 
+# so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
 tv = ["GOT",
      "Narcos",
      "Vice"]
 i = 0
 for show in tv:
-    new_show = tv[i]
-    new_show = new_show.upper()
-    tv[i] = new_show
+    new = tv[i]
+    new = new.upper()
+    tv[i] = new
     i += 1
 
 
