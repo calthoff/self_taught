ad9823f287c8201d29558952464b19748717c4df
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 19:33:08 -0800

    change example

--- python_ex245.py
+++ python_ex245.py
@@ -1,10 +1,12 @@
-
-# http://tinyurl.com/zct3q4b
+# IMPORTANT. I changed the way this example works
+# so it will fit on smaller devices. Old versions of the book have
+# a slightly different example. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
 
 class Orange:
-    def __init__(self, weight, color):
-        self.weight = weight
-        self.color = color
-        print("""Orange object
-              created!""")
+    def __init__(self, w, c):
+        self.weight = w
+        self.color = c
+        print("Created!")
