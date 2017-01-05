53859ac584d67917b785491f2129d5401e842777
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-05 12:55:22 -0800

    changes

--- python_ex272.py
+++ python_ex272.py
@@ -1,5 +1,6 @@
-
-# http://tinyurl.com/ztolacl
+# IMPORTANT. I recently made several changes to this example in the book so that it will fit on smaller screens.
+# If you have an older version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
 
 class Card:
@@ -8,37 +9,38 @@ class Card:
              "diamonds",
              "clubs"]
     values = [None, None,"2", "3",
-             "4", "5", "6", "7", "8",
-              "9", "10", "Jack",
-              "Queen", "King",
-              "Ace"]
+              "4", "5", "6", "7",
+              "8", "9", "10",
+              "Jack", "Queen",
+              "King", "Ace"]
 
-    def __init__(self, value, suit):
-        """suit and value are ints"""
-        self.value = value
-        self.suit = suit
+    def __init__(self, v, s):
+        """suit + value are ints"""
+        self.value = v
+        self.suit = s
 
-    def __lt__(self, other):
-        if self.value < other.value:
+    def __lt__(self, c2):
+        if self.value < c2.value:
             return True
-        if self.value == other.value:
-            if self.suit < other.suit:
+        if self.value == c2.value:
+            if self.suit < c2.suit:
                 return True
             else:
                 return False
         return False
 
-    def __gt__(self, other):
-        if self.value > other.value:
+    def __gt__(self, c2):
+        if self.value > c2.value:
             return True
-        if self.value == other.value:
-            if self.suit > other.suit:
+        if self.value == c2.value:
+            if self.suit > c2.suit:
                 return True
             else:
                 return False
         return False
 
     def __repr__(self):
-        v = self.values[self.value]
-        v = v + " of " + self.suits[self.suit]
+        v = self.values[self.value] +\
+            " of " + \
+            self.suits[self.suit]
         return v
