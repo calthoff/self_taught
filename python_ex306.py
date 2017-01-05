43f638974ea10be3dd092382a7235062ec3dc7e1
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-05 14:30:03 -0800

    change variable

--- python_ex306.py
+++ python_ex306.py
@@ -1,14 +1,20 @@
+# I changed the variable in this example from
+#  number in older versions of the book to n 
+# so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
-def sequential_search(number_list,
-                        number):
+
+def ss(number_list, n):
     found = False
     for i in number_list:
-        if i == number:
+        if i == n:
             found = True
             break
     return found
-nmbers = range(0, 100)
-s1 = sequential_search(nmbers, 2)
+
+numbers = range(0, 100)
+s1 = ss(numbers, 2)
 print(s1)
-s2 = sequential_search(nmbers, 202)
+s2 = ss(numbers, 202)
 print(s2)
