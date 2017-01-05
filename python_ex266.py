d6d08b3db29d395d431da0cd7b9e594d4313d4f3
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-05 12:34:13 -0800

    add message fix spacing

--- python_ex266.py
+++ python_ex266.py
@@ -1,26 +1,25 @@
-
-# http://tinyurl.com/jth53yt
+# variable name length in older editions of the book changed to len.
+# Also the variable name created was changed to recs.
+# Email cory@theselftaughtprogrammer.io for latest version
 
 
 class Rectangle():
-    created = []
-
+    recs = []
 
     def __init__(self, w, l):
         self.width = w
-        self.length = l
-        self.created.append((self.width,
-                             self.length))
+        self.len = l
+        self.recs.append((self.width,
+                          self.len))
 
     def print_size(self):
-        print("""{} by
-        {}""".format(self.width,
-                     self.length))
-
+        print("""{} by {}
+              """.format(self.width,
+                         self.len))
 
 r1 = Rectangle(10, 24)
 r2 = Rectangle(20, 40)
 r3 = Rectangle(100, 200)
 
+print(Rectangle.recs)
 
-print(Rectangle.created)
