587b9634dc7eec8350ecb462f880f5e6e4f09d7f
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 18:58:53 -0800

    change variable

--- python_ex221.py
+++ python_ex221.py
@@ -1,14 +1,19 @@
+# I changed the variable in this example from
+#  numbs in older versions of the book to nums 
+# so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
 import statistics
 
 # mean
-numbs = [1, 5, 33, 12, 46, 33, 2]
-statistics.mean(numbs)
+nums = [1, 5, 33, 12, 46, 33, 2]
+statistics.mean(nums)
 
 
 # median
-statistics.median(numbs)
+statistics.median(nums)
 
 
 # mode
-statistics.mode(numbs)
+statistics.mode(nums)
