cf2f573b13f4780f55c103274301ecbcee7fa918
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 17:22:44 -0800

    shorten

--- python_ex179.py
+++ python_ex179.py
@@ -1,6 +1,16 @@
+# I changed the variable in this example from
+#  one_string in older versions of the book to one 
+# so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
-words = ["The", "fox",
-         "jumped", "over",
-         "the", "fence", "."]
-one_string = "".join(words)
-one_string
+
+words = ["The",
+         "fox",
+         "jumped",
+         "over",
+         "the",
+         "fence",
+         "."]
+one = "".join(words)
+one
