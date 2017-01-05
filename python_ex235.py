4b12358a63160a2f81c9ec29f9fd605da2b385e7
Cory Althoff <coryalthoff@Corys-MacBook-Pro.local>
2017-01-04 19:10:32 -0800

    change variable name

--- python_ex235.py
+++ python_ex235.py
@@ -1,5 +1,9 @@
+# I changed the variable in this example from
+#  read or reader in older versions of the book to r 
+# so the example fits on smaller devices. If you have an older
+# version of the book, you can email me at cory@theselftaughtprogrammer.io 
+# and I will send you the newest version. Thank you so much for purchasing my book!
 
-# http://tinyurl.com/jpqahfb
 
 
 import csv
@@ -11,6 +15,6 @@ import csv
 
 
 with open("st.csv", "r") as f:
-    rder = csv.reader(f, delimiter=",")
-    for row in rder:
+    r = csv.reader(f, delimiter=",")
+    for row in r:
         print(",".join(row))
