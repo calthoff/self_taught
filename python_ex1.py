b54f0943da115703fdbb1ccafd2aa39cb47a4cc1
Cory Althoff <cory@theselftaughtprogrammer.io>
2017-05-08 16:28:13 -0700

    Update python_ex1.py

--- python_ex1.py
+++ python_ex1.py
@@ -1,3 +1,3 @@
-# Thank you so much for purchasing my book! Feel free to contact me at cory[at]theselftaughtprogrammer.io
+# Thank you so much for purchasing my book! Feel free to contact me at cory[at]theselftaughtprogrammer.io.
 
 print("Hello, World!")
