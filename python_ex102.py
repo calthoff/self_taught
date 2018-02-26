a2f5ff142fb036720b52c180cdb74efd044e3771
Cory Althoff <cory@theselftaughtprogrammer.io>
2018-02-26 15:52:45 -0800

    Update python_ex102.py

--- python_ex102.py
+++ python_ex102.py
@@ -1,9 +1,5 @@
-# I shortened the message that gets printed in this example.
-# I also shortened the variable names.
-# I did this so the example will fit better on smaller devices.
-# If you have an older version of the book, you can email
-# me at cory@theselftaughtprogrammer.io 
-# to update to the newest version. Thank you so much for reading!
+# Thank you so much for purchasing my book! Feel free to contact me at cory[at]theselftaughtprogrammer.io.
+# If you are enjoying it, please consider leaving a review on Amazon :). Best of luck on your programming journey!
 
 n = input("type a number")
 n = int(n)
