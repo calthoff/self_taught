afd34f38e35c3968e46ee3940569a5044a83d504
Cory Althoff <cory@theselftaughtprogrammer.io>
2018-02-26 15:51:39 -0800

    Update python_ex0.py

--- python_ex0.py
+++ python_ex0.py
@@ -1,3 +1,4 @@
-# Thank you so much for purchasing my book! Feel free to contact me at cory[at]theselftaughtprogrammer.io
+# Thank you so much for purchasing my book! Feel free to contact me at cory[at]theselftaughtprogrammer.io.
+# If you are enjoying it, don't forget to leave a review on Amazon :). Best of luck on your programming journey!
 
 print("Hello, World!")
