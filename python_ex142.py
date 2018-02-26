beb930a50939f315f8708cb57623d069b3cac691
Cory Althoff <cory@theselftaughtprogrammer.io>
2018-02-26 15:57:54 -0800

    Update python_ex142.py

--- python_ex142.py
+++ python_ex142.py
@@ -1,8 +1,4 @@
-# I changed the variable in this example from
-#  dystopia in older versions of the book to dys 
-# so the example fits on smaller devices. If you have an older
-# version of the book, you can email me at cory@theselftaughtprogrammer.io 
-# and I will send you the newest version. Thank you so much for purchasing my book!
+# Thanks so much for reading my book. Feel free to contact me at cory[at]theselftaughtprogrammer.io.
 
 dys = ("1984",
             "Brave New World",
