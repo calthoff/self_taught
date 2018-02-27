acaa886cac0f6d846bf826bee5aa83aa3dbf8a01
Cory Althoff <cory@theselftaughtprogrammer.io>
2018-02-26 16:05:48 -0800

    Update python_ex197.py

--- python_ex197.py
+++ python_ex197.py
@@ -1,15 +1,4 @@
-# IMPORTANT. I recently changed this example in the book.
-# In older versions of the book the example was
-
-#ivan = """In place of death
-#          there was light.
-#       """
-#ivan[:17]
-
-
-# I made this change so the example fits on smaller devices. If you have an older
-# version of the book, you can email me at cory@theselftaughtprogrammer.io 
-# and I will send you the newest version. Thank you so much for purchasing my book!
+# Thanks so much for reading my book. Feel free to contact me at cory[at]theselftaughtprogrammer.io.
 
 ivan = """In place of death there \
 was light."""
