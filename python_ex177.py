0944beae1bbb1f531493c13a1152def02cd63755
Cory Althoff <cory@theselftaughtprogrammer.io>
2018-02-26 16:02:58 -0800

    Update python_ex177.py

--- python_ex177.py
+++ python_ex177.py
@@ -1,11 +1,3 @@
-# IMPORTANT. I recently changed this example in the book.
-# In older versions of the book the example was
-
-#"""I jumped over the puddle.
- #  It was 12 feet!""".split(".")
-
-# I made this change so the example fits on smaller devices. If you have an older
-# version of the book, you can email me at cory@theselftaughtprogrammer.io 
-# and I will send you the newest version. Thank you so much for purchasing my book!
+# Thanks so much for reading my book. Feel free to contact me at cory[at]theselftaughtprogrammer.io.
 
 """Hello.Yes!""".split(".")
