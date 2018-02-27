f3b0b568624cc3070095266aed0d9b299870dfcc
Cory Althoff <cory@theselftaughtprogrammer.io>
2018-02-26 16:11:11 -0800

    Update python_ex244.py

--- python_ex244.py
+++ python_ex244.py
@@ -1,9 +1,4 @@
-# I changed this example to print Created. In previous examples it printed
-# Orange object created! I did this so the example fits on smaller devices.
-# If you have an older version of the book, you can email me at
-# cory@theselftaughtprogrammer.io  and I will send you the newest version.
-# Thank you so much for purchasing my book!
-
+# Thanks so much for reading my book. Feel free to contact me at cory[at]theselftaughtprogrammer.io.
 
 class Orange:
     def __init__(self):
