7e7338939ec97991709208504409ae0c95e6b97d
Cory Althoff <cory@theselftaughtprogrammer.io>
2018-02-26 16:03:50 -0800

    Update python_ex179.py

--- python_ex179.py
+++ python_ex179.py
@@ -1,8 +1,5 @@
-# I changed the variable in this example from
-#  one_string in older versions of the book to one 
-# so the example fits on smaller devices. If you have an older
-# version of the book, you can email me at cory@theselftaughtprogrammer.io 
-# and I will send you the newest version. Thank you so much for purchasing my book!
+# Thank you so much for purchasing my book! Feel free to contact me at cory[at]theselftaughtprogrammer.io.
+# If you are enjoying it, please consider leaving a review on Amazon :). Keep up the hard work!
 
 
 words = ["The",
