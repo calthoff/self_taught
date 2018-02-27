5f1cfa0b14e2a529177d2e2fc925720f1048ed93
Cory Althoff <cory@theselftaughtprogrammer.io>
2018-02-26 16:04:11 -0800

    Update python_ex181.py

--- python_ex181.py
+++ python_ex181.py
@@ -1,13 +1,4 @@
-# IMPORTANT. I recently changed this example in the book.
-# In older versions of the book the example was
-
-# sentence = "    The cat’s craddle.        "
-# sentence = sentence.strip()
-# sentence
-
-# I made this change so the example fits on smaller devices. If you have an older
-# version of the book, you can email me at cory@theselftaughtprogrammer.io 
-# and I will send you the newest version. Thank you so much for purchasing my book!
+# Thanks so much for reading my book. Feel free to contact me at cory[at]theselftaughtprogrammer.io.
 
 s = "    The        "
 s = s.strip()
