28cea13c1c011f8ee3d43bd0dae26ee699f2d52b
Cory Althoff <cory@theselftaughtprogrammer.io>
2018-02-26 16:27:47 -0800

    Update python_ex133.py

--- python_ex133.py
+++ python_ex133.py
@@ -1,5 +1,6 @@
 # Thank you so much for purchasing my book! Feel free to contact me at cory[at]theselftaughtprogrammer.io.
-# If you are enjoying it, please consider leaving a review on Amazon :). Keep up the hard work!
+# If you are enjoying it, please consider leaving a review on Amazon :). 
+# https://www.amazon.com/dp/B01M01YDQA#customerReviews. Keep up the hard work!
 
 colors1 = ["blue", "green", "yellow"]
 colors2 = ["orange", "pink", "black"]
