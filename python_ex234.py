dbd510bbb046fc26b35dff0111c69c410d6d9c26
Cory Althoff <cory@theselftaughtprogrammer.io>
2018-02-26 16:09:01 -0800

    Update python_ex234.py

--- python_ex234.py
+++ python_ex234.py
@@ -1,8 +1,4 @@
-# IMPORTANT. In some previous versions of the book, this example contains an error.
-# I've fixed the error in the newest version of the book and it is fixed below. "new_file.csv" should be
-# "st.csv"
-# If you have an older version of the book, you can email me at cory@theselftaughtprogrammer.io 
-# and I will send you the newest version. Thank you so much for purchasing my book!
+# Thanks so much for reading my book. Feel free to contact me at cory[at]theselftaughtprogrammer.io.
 
 
 import csv
