3a0ba6008a6196562e0190ce72f7d5eac28df8ed
Cory Althoff <cory@theselftaughtprogrammer.io>
2018-02-26 16:16:04 -0800

    Update python_ex272.py

--- python_ex272.py
+++ python_ex272.py
@@ -1,6 +1,5 @@
-# IMPORTANT. I recently made several changes to this example in the book so that it will fit on smaller screens.
-# If you have an older version of the book, you can email me at cory@theselftaughtprogrammer.io 
-# and I will send you the newest version. Thank you so much for purchasing my book!
+# Thank you so much for purchasing my book! Feel free to contact me at cory[at]theselftaughtprogrammer.io.
+# If you are enjoying it, please consider leaving a review on Amazon :). Keep up the hard work!
 
 
 class Card:
