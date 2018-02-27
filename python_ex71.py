b9c6a7842d389b2cda147e2c2a62d1a058ef296b
Cory Althoff <cory@theselftaughtprogrammer.io>
2018-02-26 16:22:43 -0800

    Update python_ex71.py

--- python_ex71.py
+++ python_ex71.py
@@ -1,6 +1,5 @@
-# the variable in this example has been changed from
-# country in previous versions of the book to home
-# so the example fits on smaller devices
+# Thank you so much for purchasing my book! Feel free to contact me at cory[at]theselftaughtprogrammer.io.
+# If you are enjoying it, please consider leaving a review on Amazon :). https://www.amazon.com/dp/B01M01YDQA#customerReviews. Keep up the hard work!
 
 home = "America"
 if home == "America":
