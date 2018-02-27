271c06a423871be960220a8976c4e7c45725b99e
Cory Althoff <cory@theselftaughtprogrammer.io>
2018-02-26 16:04:22 -0800

    Update python_ex182.py

--- python_ex182.py
+++ python_ex182.py
@@ -1,17 +1,4 @@
-# IMPORTANT. I recently changed this example in the book.
-# In older versions of the book the example was
-
-#equ = """All animals are
-#         equal, but some
-#         animals are more
-#         equal than others.
-#      """
-#equ = equ.replace("a", "@")
-#equ
-
-# I made this change so the example fits on smaller devices. If you have an older
-# version of the book, you can email me at cory@theselftaughtprogrammer.io 
-# and I will send you the newest version. Thank you so much for purchasing my book!
+# Thanks so much for reading my book. Feel free to contact me at cory[at]theselftaughtprogrammer.io.
 
 
 equ = "All animals are equal."
