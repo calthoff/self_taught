ac8af3262a998ecf1f511f684448fefeed7cb8a4
Cory Althoff <cory@theselftaughtprogrammer.io>
2018-02-26 16:19:29 -0800

    Update python_ex299.py

--- python_ex299.py
+++ python_ex299.py
@@ -1,10 +1,5 @@
-# IMPORTANT. I recently changed this example in the book.
-# I changed return self.items[len(self.items)-1] (in older versions) to:
-# last = len(self.items)-1
-# return self.items[last]
-# I made this change so the example fits on smaller devices. If you have an older
-# version of the book, you can email me at cory@theselftaughtprogrammer.io 
-# and I will send you the newest version. Thank you so much for purchasing my book!
+# Thank you so much for purchasing my book! Feel free to contact me at cory[at]theselftaughtprogrammer.io.
+# If you are enjoying it, please consider leaving a review on Amazon :). https://www.amazon.com/dp/B01M01YDQA#customerReviews. Keep up the hard work!
 
 
 class Stack:
