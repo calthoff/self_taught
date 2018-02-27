72c8b59f91cf12ccf67e8f36debfc076ef489306
Cory Althoff <cory@theselftaughtprogrammer.io>
2018-02-26 16:02:27 -0800

    Update python_ex174.py

--- python_ex174.py
+++ python_ex174.py
@@ -1,14 +1,4 @@
-# IMPORTANT. I recently changed this example in the book.
-# In older versions of the book the example was:
-
-#year_born = "1952"
-#"""Walter Isaacson was
-#   born in {}
-#""".format(year_born)
-
-# I made this change so the example fits on smaller devices. If you have an older
-# version of the book, you can email me at cory@theselftaughtprogrammer.io 
-# and I will send you the newest version. Thank you so much for purchasing my book!
+# Thanks so much for reading my book. Feel free to contact me at cory[at]theselftaughtprogrammer.io.
 
 last = "Faulkner"
 "William {}".format(last)
