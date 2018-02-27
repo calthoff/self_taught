59da0d3d43c748a710765f7b4e2140f1a8cc004e
Cory Althoff <cory@theselftaughtprogrammer.io>
2018-02-26 16:02:38 -0800

    Update python_ex175.py

--- python_ex175.py
+++ python_ex175.py
@@ -1,20 +1,4 @@
-# IMPORTANT. I recently changed this example in the book.
-# In older versions of the book the example was:
-
-#name = "Walter Isaacson"
-#year_born = "1952"
-#book1 = "Steve Jobs"
-#book2 = "Benjamin Franklin"
-
-#"""{} was born in {}.
-#   He's written books about
-#   {} and {}.
-#""".format(name, year_born,
-#            book1, book2)
-
-# I made this change so the example fits on smaller devices. If you have an older
-# version of the book, you can email me at cory@theselftaughtprogrammer.io 
-# and I will send you the newest version. Thank you so much for purchasing my book!
+# Thanks so much for reading my book. Feel free to contact me at cory[at]theselftaughtprogrammer.io.
 
 
 author = "William Faulkner"
