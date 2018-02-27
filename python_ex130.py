e720a8f57bccffe9693c33b2f90243b9aa3b07f3
Cory Althoff <cory@theselftaughtprogrammer.io>
2018-02-26 16:27:31 -0800

    Update python_ex130.py

--- python_ex130.py
+++ python_ex130.py
@@ -1,2 +1,6 @@
+# Thank you so much for purchasing my book! Feel free to contact me at cory[at]theselftaughtprogrammer.io.
+# If you are enjoying it, please consider leaving a review on Amazon :). 
+# https://www.amazon.com/dp/B01M01YDQA#customerReviews. Keep up the hard work!
+
 colors = ["blue","green","yellow"]
 colors[4]
