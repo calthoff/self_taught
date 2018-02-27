fb88a6eb077003bfb4c6b19b2ea2c11f3c94bde4
Cory Althoff <cory@theselftaughtprogrammer.io>
2018-02-26 16:18:10 -0800

    Update python_ex291.py

--- python_ex291.py
+++ python_ex291.py
@@ -1,7 +1,6 @@
-# IMPORTANT. I recently changed this example in the book.
-# I made changes so the example fits on smaller devices. If you have an older
-# version of the book, you can email me at cory@theselftaughtprogrammer.io 
-# and I will send you the newest version. Thank you so much for purchasing my book!
+# Thank you so much for purchasing my book! Feel free to contact me at cory[at]theselftaughtprogrammer.io.
+# If you are enjoying it, please consider leaving a review on Amazon :). Keep up the hard work!
+
 
 
 def scrape(self):
